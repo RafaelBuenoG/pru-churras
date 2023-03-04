aa2e419c6ea5bf907d5a05dea381cdda617f35fe
Rafael Bueno <rafaelbuenog2020@gmail.com>
2023-03-03 22:19:37 -0300

    Backend Truco Finalizado

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,93 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  pontos = 1;
+  time1 = 0;
+  time2 = 0;
+  vitoria1 = 0;
+  vitoria2 = 0;
+
   constructor() {}
 
+  valendo1()
+  {
+    this.pontos = 1;
+  }
+
+  valendo3()
+  {
+    this.pontos = 3;
+  }
+
+  valendo6()
+  {
+    this.pontos = 6;
+  }
+
+  valendo9()
+  {
+    this.pontos = 9;
+  }
+
+  valendo12()
+  {
+    this.pontos = 12;
+  }
+
+  soma1()
+  {
+    if (this.time1 < 12)
+    {
+      this.time1++;
+    }
+    if (this.time1 == 12)
+    {
+      this.vitoria1++
+      this.time1 = 0;
+      this.time2 = 0;
+      console.log(this.vitoria1)
+    }
+  }
+
+  soma2()
+  {
+    if (this.time2 < 12)
+    {
+      this.time2++;
+    }
+    if (this.time2 == 12)
+    {
+      this.vitoria2++
+      this.time1 = 0;
+      this.time2 = 0;
+      console.log(this.vitoria2)
+    }
+  }
+
+  subtracao1()
+  {
+    if (this.time1 > 0)
+    {
+      this.time1--;
+    }
+  }
+
+  subtracao2()
+  {
+    if (this.time2 > 0)
+    {
+      this.time2--;
+    }
+  }
+
+  limpar()
+  {
+    this.pontos = 1;
+    this.time1 = 0;
+    this.time2 = 0;
+    this.vitoria1 = 0;
+    this.vitoria2 = 0;
+  }
+
+  
 }
