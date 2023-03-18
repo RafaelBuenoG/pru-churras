870a0d108ed6a788bc362a5b1c4562ce39e5dc3d
Rafael Bueno <rafaelbuenog2020@gmail.com>
2023-03-17 22:22:14 -0300

    Tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { randomInt } from 'crypto';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  tamanho = 6;
+  valorAtual = 0;
+  imgsDados = [
+    'assets/dice-red.png', // Posição 0
+    'assets/dice.gif' // Posição 1
+  ]
+  imagemDado = this.imgsDados[0];
+
   constructor() {}
 
+  jogarDado()
+  {
+    this.valorAtual = Math.floor(Math.random() * this.tamanho) + 1;
+    this.valorAtual = 0;
+    setTimeout(() => {
+      this.imagemDado = this.imgsDados[1];
+    }, 2000);
+  }
 }
